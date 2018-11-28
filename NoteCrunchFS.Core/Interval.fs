7f40b086673314ae79966af1cdb0fae72e376672
Jonathan Roeber <r734@fastmail.com>
2018-11-27 21:52:01 -0500

    add nice Interval creation (not tested, probably riddled with bugs)

--- NoteCrunchFS.Core/Interval.fs
+++ NoteCrunchFS.Core/Interval.fs
@@ -1,11 +1,56 @@
 ﻿module NoteCrunchFS.Core.Interval
 open NoteCrunchFS.Core.Types
 
-let rawCreate distance numSemitones =
-    { basicDistance = distance; semitones = numSemitones; } // TODO this allows negative intervals...should I return Interval Option instead of just Interval?
+let rawCreate distance numSemitones = // TODO should I just take the absolute value instead--to avoid using Option type?
+    match distance >= 0, numSemitones >= 0 with
+    | true, true -> Some { basicDistance = distance; semitones = numSemitones; }
+    | _   , _    -> None
 
 let createWithOffset (offset:int) (quality:BasicInterval) (intervalNumber:int) =
-    { basicDistance = 0; semitones = 0 } // TODO implement
+    let distance = intervalNumber - 1
+    if distance < 0 then None else
+
+    let semitonesInInterval quality distance =
+        let semitoneFoundation = (distance / 7) * 12
+        match quality, (distance % 7) + 1 with      // (distance % 7) + 1 brings compound interval numbers down to simple, e.g. 13 -> 6
+        | 'P', 1 -> Some semitoneFoundation         // Interval equivalence class P1 (includes P8, P15, etc.)
+        | 'P', 4 -> Some (semitoneFoundation + 5)   // Equivalence class P4, and so on
+        | 'P', 5 -> Some (semitoneFoundation + 7)
+        | 'P', _ -> None
+
+        | 'm', 2 -> Some (semitoneFoundation + 1)
+        | 'm', 3 -> Some (semitoneFoundation + 3)
+        | 'm', 6 -> Some (semitoneFoundation + 8)
+        | 'm', 7 -> Some (semitoneFoundation + 10)
+        | 'm', _ -> None
+
+        | 'M', 2 -> Some (semitoneFoundation + 2)
+        | 'M', 3 -> Some (semitoneFoundation + 4)
+        | 'M', 6 -> Some (semitoneFoundation + 9)
+        | 'M', 7 -> Some (semitoneFoundation + 11)
+        | 'M', _ -> None
+
+        | 'd', 1 -> if (semitoneFoundation >= 12) then Some (semitoneFoundation - 1) else None // d1 not valid, but d8, d15, etc. *are* valid
+        | 'd', 2 -> Some semitoneFoundation
+        | 'd', 3 -> Some (semitoneFoundation + 2)
+        | 'd', 4 -> Some (semitoneFoundation + 4)
+        | 'd', 5 -> Some (semitoneFoundation + 6)
+        | 'd', 6 -> Some (semitoneFoundation + 7)
+        | 'd', 7 -> Some (semitoneFoundation + 9)
+
+        | 'a', 1 -> Some (semitoneFoundation + 1)
+        | 'a', 2 -> Some (semitoneFoundation + 3)
+        | 'a', 3 -> Some (semitoneFoundation + 5)
+        | 'a', 4 -> Some (semitoneFoundation + 6)
+        | 'a', 5 -> Some (semitoneFoundation + 8)
+        | 'a', 6 -> Some (semitoneFoundation + 10)
+        | 'a', 7 -> Some (semitoneFoundation + 12)
+        |  _ , _ -> None
+
+    match semitonesInInterval (LanguagePrimitives.EnumToValue quality) distance with
+    | Some result -> if (result + offset < 0) then None 
+                     else Some { basicDistance = distance; semitones = result + offset }
+    | None -> None
 
 let create = createWithOffset 0
 
@@ -46,4 +91,6 @@ let toString interval =
         | "P", offset when offset > 1 -> "(P+" + offset.ToString() + ")"
         |  _ ,  _ -> "(???)"
 
-    sprintf "%s%s" (qualityString (interval.basicDistance |> baseQuality, interval |> offset)) ((interval.basicDistance + 1).ToString())
+    match interval with
+    | Some interval -> sprintf "%s%s" (qualityString (interval.basicDistance |> baseQuality, interval |> offset)) ((interval.basicDistance + 1).ToString())
+    | None -> sprintf "(invalid interval)"
