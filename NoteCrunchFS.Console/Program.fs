1e444d6edda6b9058797bccb33c414fe947c1304
Jonathan Roeber <r734@fastmail.com>
2018-11-28 21:55:27 -0500

    started on Note

--- NoteCrunchFS.Console/Program.fs
+++ NoteCrunchFS.Console/Program.fs
@@ -1,9 +1,15 @@
 ﻿open System
 open NoteCrunchFS.Core
+open NoteCrunchFS.Core.Types
 
 [<EntryPoint>]
 let main argv =
     let created = Interval.create 'm' 10
     let another = Interval.create 'a' 1
     printfn "%s" (Interval.toString (created + another))
+
+    let noteA = Note.create BasicNote.E 0 -50
+    let noteB = Note.create BasicNote.D 0 0
+
+    printfn "%b" (noteA < noteB)
     0
--- NoteCrunchFS.Core/Note.fs
+++ NoteCrunchFS.Core/Note.fs
@@ -2,5 +2,23 @@
 module NoteCrunchFS.Core.Note
 open NoteCrunchFS.Core.Types
 
-let create baseNote offset octave =
-    { baseNote = baseNote; offset = offset; octave = octave }
+let create inBaseNote inOffset inOctave =
+    { baseNote = inBaseNote; offset = inOffset; octave = inOctave }
+
+let private semitonesRightOfA0 note =
+    0
+
+let private basicDistance noteA noteB =
+    0
+
+let private semitoneDistance noteA noteB =
+    0
+
+let private getHigherNote interval =
+    0
+
+let private getLowerNote interval =     // TODO Will need to avoid code duplication with getHigherNote
+    0
+
+// let (<=) noteA noteB =
+//     true
