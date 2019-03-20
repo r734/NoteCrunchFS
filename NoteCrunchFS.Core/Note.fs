02929f3d0e6cb42d674fe29df0fb5787f5d2531b
Jonathan Roeber <r734@fastmail.com>
2019-03-19 20:56:33 -0400

    make functions private (temporarily), add convenience interactive script

--- NoteCrunchFS.Core/Note.fs
+++ NoteCrunchFS.Core/Note.fs
@@ -7,7 +7,7 @@ module Note =
     let create inBaseNote inOffset inOctave =
         { baseNote = inBaseNote; offset = inOffset; octave = inOctave }
 
-    let private basicNoteSemitones (baseNote: BasicNote) =
+    let basicNoteSemitones (baseNote: BasicNote) =
         match baseNote with
         | BasicNote.A -> 0
         | BasicNote.B -> 2
@@ -18,23 +18,23 @@ module Note =
         | BasicNote.G -> 10
         | _ -> raise (Exception "Impossible error")
 
-    let private semitonesRightOfA0 (note: Note): int =
+    let semitonesRightOfA0 (note: Note): int =
         let semitonesFromOctaves numOctaves = numOctaves * 12
         (semitonesFromOctaves note.octave) + note.offset + (basicNoteSemitones note.baseNote)
 
-    let private basicNotesRightOfA0 (note: Note): int =
+    let basicNotesRightOfA0 (note: Note): int =
         (note.octave * 7) + (int note.baseNote)
 
-    let private basicDistance noteA noteB =
+    let basicDistance noteA noteB =
         abs (basicNotesRightOfA0 noteA - basicNotesRightOfA0 noteB)
 
-    let private semitoneDistance noteA noteB =
+    let semitoneDistance noteA noteB =
         abs (semitonesRightOfA0 noteA - semitonesRightOfA0 noteB)
 
-    let private getHigherNote interval =
+    let getHigherNote note interval =
         0
 
-    let private getLowerNote interval =     // TODO Will need to avoid code duplication with getHigherNote
+    let getLowerNote note interval =     // TODO Will need to avoid code duplication with getHigherNote
         0
 
     // let (<=) noteA noteB =
---
+++ interactive.fsx
@@ -0,0 +1,5 @@
+#r "./NoteCrunchFS.Core/bin/Debug/netstandard2.0/NoteCrunchFS.Core.dll"
+open NoteCrunchFS.Core
+
+let a4 = Note.create BasicNote.A 0 4
+let c4 = Note.create BasicNote.C 0 4
